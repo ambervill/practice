192ef67cb87e44e0c569894995402b2a9a5719ee
Andrew Alexeychuk <alexejchuk@gmail.com>
2019-02-01 13:19:39 +0300

    '01.02.19'

--- cube/cube_algorithms.rb
+++ cube/cube_algorithms.rb
@@ -2,17 +2,18 @@ require './cube'
 
 cube = Cube.new
 distribution = {}
-3000.times do
-  counter = 0
-  algorithm = Algorithm.generate(41)
-  begin
-    cube.algorithm algorithm
-    counter += 1
-  end while !cube.solved?
-  distribution[counter] ||= 0
-  distribution[counter] += 1
+(2..99).each do |alg_length|
+  100.times do
+    counter = 0
+    algorithm = Algorithm.generate(alg_length)
+    begin
+      cube.algorithm algorithm
+      counter += 1
+    end while !cube.solved?
+    distribution[counter] ||= 0
+    distribution[counter] += 1
+  end
 end
-
 distribution.keys.sort.each do |key|
   print key, "=", distribution[key], "\n"
 end
